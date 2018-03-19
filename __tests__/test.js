505aa99333871699531cf42c54beee98f8be7c61
deadmp3 <deadmp3@gmail.com>
2018-03-19 22:37:13 +0300

    fix test

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,12 +2,23 @@ import nock from 'nock';
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
 
+import os from 'os';
 import fs from 'fs';
 import path from 'path';
+import rimraf from 'rimraf';
 
 import loader from '../src';
 
 axios.defaults.adapter = httpAdapter;
+
+let dir = '';
+
+beforeEach(() => {
+  dir = fs.mkdtempSync(path.join(os.tmpdir(), './'));
+});
+afterEach(() => {
+  rimraf(dir, () => {});
+});
 
 test('save page', () => {
   const host = 'http://test.com';
@@ -16,7 +27,6 @@ test('save page', () => {
     .reply(200, fs.readFileSync(path.resolve('__tests__/__fixtures__/page.html'), 'utf8'));
 
   const page = `${host}/page`;
-  const dir = '/home/deadmp3/projects/page-loader/tmp';
 
   return loader(page, dir)
     .then(() => fs.readFileSync(`${dir}/index.html`, 'utf8'))
