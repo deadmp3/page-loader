c36a086c0f368d1da9fb750be17038a9e1d8cc17
deadmp3 <deadmp3@gmail.com>
2018-03-19 22:11:23 +0300

    add loader

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
-import pageLoader from '..';
+import loader from '..';
 import { version } from '../../package.json';
 
 program
   .version(version)
   .arguments('<url>')
-  .option('--output [dir]', 'Output dir', '/var/tmp')
-  .action((url, options) => {
-    pageLoader(url, options.output);
-  });
+  .option('--output [dir]', 'Output dir', process.cwd())
+  .action((url, options) => loader(url, options.output));
 
 program.parse(process.argv);
