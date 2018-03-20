1015c0e86667849ff9ac53a5a203129d91346f3a
deadmp3 <deadmp3@gmail.com>
2018-03-20 22:45:38 +0300

    add async test

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
 
 import os from 'os';
-import fs from 'fs';
+import fs from 'mz/fs';
 import path from 'path';
 import rimraf from 'rimraf';
 
@@ -20,7 +20,7 @@ afterEach(() => {
   rimraf(dir, () => {});
 });
 
-test('save page', () => {
+test('save page', async () => {
   const host = 'http://test.com';
   nock(host)
     .get('/page')
@@ -28,7 +28,29 @@ test('save page', () => {
 
   const page = `${host}/page`;
 
-  return loader(page, dir)
-    .then(() => fs.readFileSync(`${dir}/index.html`, 'utf8'))
-    .then(data => expect(data).toBe(fs.readFileSync(path.resolve('__tests__/__fixtures__/page.html'), 'utf8')));
+  await loader(page, dir);
+  const [loadData, checkData] = await Promise.all([fs.readFile(`${dir}/test-com-page.html`, 'utf8'),
+    fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8')]);
+  expect(loadData).toBe(checkData);
+
+  /* await loader(page, dir);
+  const loadData = await fs.readFile(`${dir}/index.html`, 'utf8');
+  const checkData = await fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
+  expect(loadData).toBe(checkData); */
+
+  /* return loader(page, dir)
+    .then(() => {
+      const loadData = fs.readFile(`${dir}/index.html`, 'utf8');
+      const checkData = fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
+      return Promise.all([loadData, checkData]);
+    })
+    .then(([loadData, checkData]) => expect(loadData).toBe(checkData)); */
+
+  /* return loader(page, dir)
+    .then(() => fs.readFile(`${dir}/index.html`, 'utf8'))
+    .then((loadData) => {
+      const readCheckData = fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
+      return Promise.all([loadData, readCheckData]);
+    })
+    .then(([loadData, checkData]) => expect(loadData).toBe(checkData)); */
 });
