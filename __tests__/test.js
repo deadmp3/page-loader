ff507afd829fbebb564f3f32b68d66cf40573d32
deadmp3 <deadmp3@gmail.com>
2018-03-27 23:04:38 +0300

    add test for files src

--- __tests__/test.js
+++ __tests__/test.js
@@ -20,16 +20,32 @@ afterEach(async () => {
   await rimraf(dir, () => {});
 });
 
+const compare = async (path1, path2) => {
+  const [file1, file2] = await Promise.all([fs.readFile(path1, 'utf8'),
+    fs.readFile(path2, 'utf8')]);
+  expect(file1).toBe(file2);
+};
+
 test('save page', async () => {
   const host = 'http://test.com';
+  const pathPage = path.join(__dirname, '__fixtures__/page.html');
+  const pathFavicon = path.join(__dirname, '__fixtures__/favicon.ico');
+  const pathRuby = path.join(__dirname, '__fixtures__/ruby.png');
   nock(host)
     .get('/page')
-    .reply(200, await fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8'));
+    .reply(200, await fs.readFile(pathPage, 'utf8'));
+  nock(host)
+    .get('/assets/icons/default/favicon.ico')
+    .reply(200, await fs.readFile(pathFavicon));
+  nock(host)
+    .get('/images/ruby.png')
+    .reply(200, await fs.readFile(pathRuby));
 
   const page = `${host}/page`;
+  const pathFiles = 'test-com-page_files';
 
   await loader(page, dir);
-  const [loadData, checkData] = await Promise.all([fs.readFile(`${dir}/test-com-page.html`, 'utf8'),
-    fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8')]);
-  expect(loadData).toBe(checkData);
+  await compare(path.join(dir, 'test-com-page.html'), pathPage);
+  await compare(path.join(dir, pathFiles, 'assets-icons-default-favicon.ico'), pathFavicon);
+  await compare(path.join(dir, pathFiles, 'images-ruby.png'), pathRuby);
 });
