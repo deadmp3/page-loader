dac93dc37ec2b57c5b0968e2e002de4d6b17cdae
deadmp3 <deadmp3@gmail.com>
2018-03-27 22:18:56 +0300

    add create name

--- __tests__/test.js
+++ __tests__/test.js
@@ -13,18 +13,18 @@ axios.defaults.adapter = httpAdapter;
 
 let dir = '';
 
-beforeEach(() => {
-  dir = fs.mkdtempSync(path.join(os.tmpdir(), './'));
+beforeEach(async () => {
+  dir = await fs.mkdtemp(path.join(os.tmpdir(), './'));
 });
-afterEach(() => {
-  rimraf(dir, () => {});
+afterEach(async () => {
+  await rimraf(dir, () => {});
 });
 
 test('save page', async () => {
   const host = 'http://test.com';
   nock(host)
     .get('/page')
-    .reply(200, fs.readFileSync(path.resolve('__tests__/__fixtures__/page.html'), 'utf8'));
+    .reply(200, await fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8'));
 
   const page = `${host}/page`;
 
@@ -32,25 +32,4 @@ test('save page', async () => {
   const [loadData, checkData] = await Promise.all([fs.readFile(`${dir}/test-com-page.html`, 'utf8'),
     fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8')]);
   expect(loadData).toBe(checkData);
-
-  /* await loader(page, dir);
-  const loadData = await fs.readFile(`${dir}/index.html`, 'utf8');
-  const checkData = await fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
-  expect(loadData).toBe(checkData); */
-
-  /* return loader(page, dir)
-    .then(() => {
-      const loadData = fs.readFile(`${dir}/index.html`, 'utf8');
-      const checkData = fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
-      return Promise.all([loadData, checkData]);
-    })
-    .then(([loadData, checkData]) => expect(loadData).toBe(checkData)); */
-
-  /* return loader(page, dir)
-    .then(() => fs.readFile(`${dir}/index.html`, 'utf8'))
-    .then((loadData) => {
-      const readCheckData = fs.readFile(path.resolve('__tests__/__fixtures__/page.html'), 'utf8');
-      return Promise.all([loadData, readCheckData]);
-    })
-    .then(([loadData, checkData]) => expect(loadData).toBe(checkData)); */
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 import fs from 'mz/fs';
+import { URL } from 'url';
+
+const getName = (url) => {
+  const myURL = new URL(url);
+  return `${myURL.hostname}${myURL.pathname}`
+    .replace(/(\/|\.)/g, '-');
+};
 
 export default (url, dir) =>
   axios.get(url)
-    .then(res => fs.writeFile(`${dir}/index.html`, res.data))
+    .then(res => fs.writeFile(`${dir}/${getName(url)}.html`, res.data))
     .catch((err) => {
       console.log(err);
     });
