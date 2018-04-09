3aef845d48f10b1c166727c3725c8c08c9665e57
deadmp3 <deadmp3@gmail.com>
2018-04-09 22:10:23 +0300

    refactor async await

--- src/index.js
+++ src/index.js
@@ -4,42 +4,68 @@ import path from 'path';
 import getLinks from './parser';
 import { getLocalLink, pageName, catalogName, isLocalLink, fullLink } from './names';
 
-const saveLocalLinks = (getLoadLinks, dirFiles) =>
-  getLoadLinks.then((loadLinks) => {
-    if (loadLinks.length === 0) {
-      return [];
-    }
-    fs.mkdir(dirFiles);
-    return Promise.all(loadLinks.map(({ link, localLink }) =>
-      axios({
-        method: 'get',
-        url: link,
-        responseType: 'arraybuffer',
-      }).then(({ data }) => fs.writeFile(localLink, data))));
-  });
+// const saveLocalLinks = (getLoadLinks, dirFiles) =>
+//   getLoadLinks.then((loadLinks) => {
+//     if (loadLinks.length === 0) {
+//       return [];
+//     }
+//     fs.mkdir(dirFiles);
+//     return Promise.all(loadLinks.map(({ link, localLink }) =>
+//       axios({
+//         method: 'get',
+//         url: link,
+//         responseType: 'arraybuffer',
+//       }).then(({ data }) => fs.writeFile(localLink, data))));
+//   });
 
-export default (url, dir) => {
+export default async (url, dir) => {
   const catalog = catalogName(url);
 
-  const getHtml = axios.get(url).then(res => res.data);
-  const getLocalLinks = getHtml.then(html =>
-    getLinks(html).filter(isLocalLink));
-  const getExchangeLinks = getLocalLinks.then(localLinks =>
-    localLinks.map(link => ({ link, localLink: getLocalLink(link, catalog) })));
-  const getLocalHtml = Promise.all([getHtml, getExchangeLinks]).then(([html, exchangeLinks]) =>
-    exchangeLinks.reduce((str, { link, localLink }) => str.replace(new RegExp(link, 'g'), localLink), html));
-  const getSaveHtml = getLocalHtml.then(html => fs.writeFile(path.join(dir, pageName(url)), html));
+  // const getHtml = axios.get(url).then(res => res.data);
 
+  const { data: html } = await axios.get(url);
+
+  // const getLocalLinks = getHtml.then(html =>
+  //   getLinks(html).filter(isLocalLink));
+  const localLinks = getLinks(html).filter(isLocalLink);
+  // const getExchangeLinks = getLocalLinks.then(localLinks =>
+  //   localLinks.map(link => ({ link, localLink: getLocalLink(link, catalog) })));
+  const exchangeLinks = localLinks.map(link => ({ link, localLink: getLocalLink(link, catalog) }));
+  // const getLocalHtml = Promise.all([getHtml, getExchangeLinks]).then(([html, exchangeLinks]) =>
+  //   exchangeLinks.reduce((str, { link, localLink }) => str.replace(new RegExp(link, 'g'), localLink), html));
+  const localHtml = exchangeLinks.reduce((str, { link, localLink }) => str.replace(new RegExp(link, 'g'), localLink), html);
+  // const getSaveHtml = getLocalHtml.then(html => fs.writeFile(path.join(dir, pageName(url)), html));
+  await fs.writeFile(path.join(dir, pageName(url)), localHtml);
+
+  // const dirFiles = path.join(dir, catalog);
   const dirFiles = path.join(dir, catalog);
-  const getLoadLinks = getLocalLinks.then(localLinks =>
-    localLinks.map(link => ({
-      link: fullLink(link, url),
-      localLink: getLocalLink(link, dirFiles),
-    })));
-  const getSaveLocalLinks = saveLocalLinks(getLoadLinks, dirFiles);
 
-  return Promise.all([getSaveHtml, getSaveLocalLinks])
-    .catch((err) => {
-      console.log(err);
+  // const getLoadLinks = getLocalLinks.then(localLinks =>
+  //   localLinks.map(link => ({
+  //     link: fullLink(link, url),
+  //     localLink: getLocalLink(link, dirFiles),
+  //   })));
+  const loadLinks = localLinks.map(link => ({
+    link: fullLink(link, url),
+    localLink: getLocalLink(link, dirFiles),
+  }));
+  // const getSaveLocalLinks = saveLocalLinks(getLoadLinks, dirFiles);
+
+  if (loadLinks.length !== 0) {
+    fs.mkdir(dirFiles);
+  }
+  loadLinks.map(async ({ link, localLink }) => {
+    const { data } = await axios({
+      method: 'get',
+      url: link,
+      responseType: 'arraybuffer',
     });
+    await fs.writeFile(localLink, data);
+  });
+
+
+  // return Promise.all([getSaveHtml, getSaveLocalLinks])
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 };
